d5c7a5fe4ab6d0f3a4436a84d2c98be030e1cb67
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-06-05 16:21:48 -0300

    News page

---
+++ src/pages/News/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import Background from '../../components/Background'
+// import { Container } from './styles';
+
+const News = () => {
+  return (
+    <Background>
+      <Text>Página de notícias</Text>
+    </Background>
+  );
+}
+
+export default News;
