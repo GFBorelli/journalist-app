67718196c419820502f7a1e294a8554338bdf58c
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-06-05 14:59:13 -0300

    Starting

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Text>Edit</Text>
+    </>
+  );
+};
+
+export default App;
