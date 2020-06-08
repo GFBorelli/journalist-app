a11ee1bb0118c0b93084fab802eea9aaac059a21
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-06-08 11:47:23 -0300

    Register page with API consuption

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,21 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator } from 'react-native'
 
 import Background from '../../components/Background'
-import { Container, Button, Text } from './styles';
+import { Container, Form, NewsTitle, NewsContent, UserPicker, Button, ButtonText } from './styles';
 
 const register = ({ navigation }) => {
+  const [loading, setLoading] = useState(true)
+  const [authorList, setAuthorList] = useState('');
+  const [selectedAuthor, setSelectedAuthor] = useState('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => {
+        setAuthorList(json)
+        setLoading(false)
+      })
+  }, [])
 
   return (
     <Background>
-      <Container>
+      {loading ?
+        <Container style={{ flex: 1, justifyContent: "center" }}>
+          <ActivityIndicator color='#FFF' size={80} />
+        </Container>
+        :
+        <Container>
 
-        <Button onPress={() => navigation.navigate('News')}>
-          <Text>Ir para notícias</Text>
-        </Button>
+          <Form>
+            <UserPicker
+              selectedValue={selectedAuthor}
+              onValueChange={(author) => setSelectedAuthor(author)}
+            >
+              <UserPicker.Item key="default" label="Escolha um autor" value="default" />
+              {authorList.map((author) => (
+                <UserPicker.Item key={author.id} label={author.name} value={author.name} />
+              ))}
 
-        <Text>Pagina cadastro de notícias</Text>
+            </UserPicker>
 
-      </Container>
+            <NewsTitle
+              autoCorrect={false}
+              autoCaptalize='none'
+              placeholder='Título da matéria'
+              returnKeyType='next'
+            />
+            <NewsContent
+              autoCorrect={false}
+              autoCaptalize='none'
+              placeholder='Conteúdo da matéria'
+              returnKeyType='next'
+              multiline={true}
+            />
+          </Form>
+
+          <Button onPress={() => { }}>
+            <ButtonText>Publicar notícia</ButtonText>
+          </Button>
+
+          <Button onPress={() => navigation.navigate('News')}>
+            <ButtonText>Ir para notícias</ButtonText>
+          </Button>
+
+        </Container>}
     </Background>
   );
 }
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,14 +1,64 @@
 import styled from 'styled-components/native';
+import { Picker } from '@react-native-community/picker'
 import { RectButton } from 'react-native-gesture-handler'
 
 export const Container = styled.View`
-  
+  align-items: center;
+
 `;
 
+export const Form = styled.View`
+    margin: 30px 0px;
+    padding: 5px;
+    background: rgba(0, 0, 0, 0.6);
+    border-radius: 4px;
+    width: 350px;
+    height: 400px;
+`
+
+export const NewsTitle = styled.TextInput.attrs({
+    placeholderTextColor: 'rgba(0, 0, 0, 0.8)'
+})`
+    background: #fff;
+    flex: 1;
+    font-size: 15px;
+    margin: 10px;
+    color: #000;
+    border-radius: 4px;
+`
+export const NewsContent = styled.TextInput.attrs({
+    placeholderTextColor: 'rgba(0, 0, 0, 0.8)'
+})`
+    background: #fff;
+    flex: 10;
+    font-size: 15px;
+    margin: 10px;
+    color: #000;
+    border-radius: 4px;
+`
+
+export const UserPicker = styled(Picker)`
+    flex: 2;
+    background: #fff;
+    font-size: 15px;
+    margin: 10px;
+    color: #000;
+    border-radius: 4px;
+`
+
 export const Button = styled(RectButton)`
-
+    padding: 15px;
+    margin-top: 15px;
+    background: #000;
+    width: 200px;
+    align-items: center;
+    border-radius: 5px;
 `
-export const Text = styled.Text`
 
+export const ButtonText = styled.Text`
+    color: #fff;
+    font-weight: bold;
+    font-size: 20px;
 `
+
 
